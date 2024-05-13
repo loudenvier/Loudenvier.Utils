ad17b37e77b3821f6d06f070c4a4d2400bb71ecb
Felipe Machado <loudenvier@gmail.com>
2024-05-13 15:22:25 -0300

    added Utils.Json project
    Added upper case naming conventions for both System.Text.Json and Newtonsoft.Json

---
+++ Loudenvier.Utils/Loudenvier.Utils.Json/UpperCaseNamingPolicy.cs
@@ -0,0 +1,7 @@
+﻿namespace System.Text.Json
+{
+    public sealed class UpperCaseNamingPolicy : JsonNamingPolicy
+    {
+        public override string ConvertName(string name) => name.ToUpperInvariant();
+    }
+}
