2becfeccd0836697889a0fefcd6e499ecb5352d4
Felipe Machado <loudenvier@gmail.com>
2024-09-09 16:03:19 -0300

    Added CaseConvention and related extensions

---
+++ Loudenvier.Utils/Loudenvier.Utils.Tests/CaseConventionsTests.cs
@@ -0,0 +1,63 @@
+﻿using Loudenvier.Utils;
+
+namespace Loudenvier.Utils.Tests
+{
+    public class CaseConventionsTests
+    {
+        [Fact]
+        public void ToCamelCase_WorksWithBasicCase() =>
+            Assert.Equal("camelCase", CaseConvention.Camel.ApplyCase("Camel-case"));
+
+        [Fact]
+        public void ToCamelCaseWorks_WithComplexCase() =>
+            Assert.Equal("camelCaseTextCommand", CaseConvention.Camel.ApplyCase("Camel-case---Text-----command"));
+
+        [Fact]
+        public void ToPascalCase_WorksWithBasicCase() =>
+            Assert.Equal("PascalCase", CaseConvention.Pascal.ApplyCase("pascal-Case"));
+
+        [Fact]
+        public void ToPascalCaseWorks_WithComplexCase() =>
+            Assert.Equal("PascalCaseTextCommand", CaseConvention.Pascal.ApplyCase("pascal-Case---text-----command"));
+
+        [Fact]
+        public void ToKebabCase_WorksWithBasicCase() =>
+            Assert.Equal("kebab-case", CaseConvention.Kebab.ApplyCase("KebabCase"));
+        [Fact]
+        public void ToKebabCase_DontRemoveDashesInOriginalString() =>
+            Assert.Equal("--kebab---case", CaseConvention.Kebab.ApplyCase("--Kebab---Case"));
+        [Fact]
+        public void ToKebabCase_DoNotSeparateRepeatedCapitals() =>
+            Assert.Equal("kebab-case-all", CaseConvention.Kebab.ApplyCase("KebabCaseALL"));
+        [Fact]
+        public void ToSnakeCase_WorksWithBasicCase() =>
+            Assert.Equal("snake_case", CaseConvention.Snake.ApplyCase("SnakeCase"));
+        [Fact]
+        public void ToSnakeCase_DoNotSeparateRepeatedCapitals() =>
+            Assert.Equal("snake_case_all", CaseConvention.Snake.ApplyCase("SnakeCaseALL"));
+        [Fact]
+        public void ToSnakeCase_DontRemoveDashesInOriginalString() =>
+            Assert.Equal("__snake___case", CaseConvention.Snake.ApplyCase("__Snake___Case"));
+        [Fact]
+        public void ToConstantCase_WorksWithBasicCase() =>
+            Assert.Equal("CONSTANT_CASE", CaseConvention.Constant.ApplyCase("ConstantCase"));
+        [Fact]
+        public void ToConstantCase_DoNotSeparateRepeatedCapitals() =>
+            Assert.Equal("CONSTANT_CASE_ALL", CaseConvention.Constant.ApplyCase("ConstantCaseAll"));
+        [Fact]
+        public void ToConstantCase_DontRemoveDashesInOriginalString() =>
+            Assert.Equal("__CONSTANT___CASE", CaseConvention.Constant.ApplyCase("__Constant___Case"));
+        [Fact]
+        public void ToCobalCase_WorksWithBasicCase() =>
+            Assert.Equal("COBOL-CASE", CaseConvention.Cobol.ApplyCase("CobolCase"));
+        [Fact]
+        public void ToCobolCase_DoNotSeparateRepeatedCapitals() =>
+            Assert.Equal("COBOL-CASE-ALL", CaseConvention.Cobol.ApplyCase("CobolCaseAll"));
+        [Fact]
+        public void ToCobolCase_DontRemoveDashesInOriginalString() =>
+            Assert.Equal("--COBOL---CASE", CaseConvention.Cobol.ApplyCase("--Cobol---Case"));
+        [Fact]
+        public void ApplyUnknownCaseConventionThrowsArgumentOutOfRangeException() =>
+            Assert.Throws<ArgumentOutOfRangeException>(() => ((CaseConvention)1000).ApplyCase("ThrowsException"));
+    }
+}
---
+++ Loudenvier.Utils/Loudenvier.Utils/Strings/CaseConvention.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Loudenvier.Utils;
+
+/// <summary>
+/// Aggregate various possible (programming) case conventions (camel case, pascal case, etc.) including some exoteric ones.
+/// </summary>
+public enum CaseConvention { 
+    /// <summary>Keep the case intact when processing</summary>
+    Original, 
+    /// <summary>Lowercases all characaters and remove common separators ('_', '-') (ex.: Flat-Case => flatcase)</summary>
+    Flat, 
+    /// <summary>The beginning char and every first char after a separator is in uppercase (ex.: pascal-case => PascalCase)</summary>
+    Pascal, 
+    /// <summary>The first char is lowercase and every other first char after a separator is in uppercase 
+    /// (ex.: Camel_case => camelCase)</summary>
+    Camel, 
+    /// <summary>Every uppercase characater is converted to lowercase and the first in a sequence is prepended with '-'
+    /// (ex.: KebabCase => kebab-case)</summary>
+    Kebab,
+    /// <summary>Every uppercase characater is converted to lowercase and the first in a sequence is prepended with '_'
+    /// (ex.: SnakeCase => snake_case)</summary>
+    Snake, 
+    /// <summary>
+    /// Uppercases all characters and prepends every original first upper case in a sequence with '_'
+    /// (ex.: ConstantCase => CONSTANT_CASE)
+    /// </summary>
+    Constant,
+    /// <summary>
+    /// Uppercases all characters and prepends every original first upper case in a sequence with '-'
+    /// (ex.: CobolCase => COBOL-CASE)
+    /// </summary>
+    Cobol,
+}
+
+public static class CaseConventionExtensions {
+
+    const char snake = '_';
+    const char kebab = '-';
+    static readonly char[] separators = [snake, kebab];
+    
+    public static string ApplyCase(this CaseConvention cmdCase, string s) {
+        return cmdCase switch {
+            CaseConvention.Original => s,
+            CaseConvention.Flat => s.StripChars(separators).ToLowerInvariant(),
+            CaseConvention.Pascal => s.ToPascalCase(),
+            CaseConvention.Camel => s.ToCamelCase(),
+            CaseConvention.Kebab => s.ToKebabCase(),
+            CaseConvention.Snake => s.ToSnakeCase(),
+            CaseConvention.Constant => s.ToConstantCase(),
+            CaseConvention.Cobol => s.ToCobolCase(),
+            _ => throw new ArgumentOutOfRangeException($"This CommandCase is not supported: {cmdCase}", nameof(cmdCase)),
+        };
+    }
+
+    static string ToCase(this string s, Func<char, char> firstCase, Func<char, char> middleCase) {
+        if (string.IsNullOrEmpty(s)) return s;
+        var parts = s.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+        parts[0] = $"{firstCase(parts[0][0])}{parts[0][1..]}";
+        if (parts.Length == 1)
+            return parts[0];
+        var combined = parts.Skip(1).Select(p => $"{middleCase(p[0])}{p[1..]}");
+        return $"{parts[0]}{string.Concat(combined)}";
+    }
+
+    static string ToCaseWithSeparator(this string s, char separator = '-') {
+        var sb = new StringBuilder(s.Length);
+        bool? lastIsUpper = null;
+        bool lastIsSep = false;
+        foreach (var ch in s) {
+            if (lastIsUpper == false && !lastIsSep && char.IsUpper(ch))
+                sb.Append(separator);
+            sb.Append(char.ToLowerInvariant(ch));
+            lastIsUpper = char.IsUpper(ch);
+            lastIsSep = ch == separator;
+        }
+        return sb.ToString();
+    }
+
+    static string ToPascalCase(this string s)
+        => ToCase(s, char.ToUpperInvariant, char.ToUpperInvariant);
+    static string ToCamelCase(this string s)
+        => ToCase(s, char.ToLowerInvariant, char.ToUpperInvariant);
+    static string ToKebabCase(this string s) => ToCaseWithSeparator(s);
+    static string ToSnakeCase(this string s) => ToCaseWithSeparator(s, '_');
+    static string ToConstantCase(this string s) => ToSnakeCase(s).ToUpperInvariant();
+    static string ToCobolCase(this string s) => ToKebabCase(s).ToUpperInvariant();
+
+}
