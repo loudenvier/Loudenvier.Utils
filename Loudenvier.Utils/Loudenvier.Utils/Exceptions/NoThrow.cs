fb5d7260d1b8baaf118ea3eddf6c8153ac21d2a5
Felipe Machado <loudenvier@gmail.com>
2024-09-23 18:55:06 -0300

    Added NoThrow unit

---
+++ Loudenvier.Utils/Loudenvier.Utils/Exceptions/NoThrow.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loudenvier.Utils;
+
+public static class NoThrow
+{
+    public static void Do(Action action) {
+        try {
+            action();
+        } catch { }
+    }
+}
