a0fe71ac9899bd2868e68c0c512ba6c74bc7222e
Felipe Machado <loudenvier@gmail.com>
2024-11-01 18:27:28 -0300

    Added EventsExtensions (RemoveEventHandlers method)

---
+++ Loudenvier.Utils/Loudenvier.Utils.Tests/EventsExtensionsTests.cs
@@ -0,0 +1,80 @@
+﻿using Loudenvier.Utils.Events;
+
+namespace Loudenvier.Utils.Tests;
+
+public class EventsExtensionsTests {
+    [Fact]
+    public void EventClassHasHandlerBehavesAsExpected() {
+        var e = new EventClass();
+        Assert.False(e.HasHandler);
+        e.MyEvent += Handler;
+        e.MyEvent += Handler2;
+        Assert.True(e.HasHandler);
+        e.MyEvent -= Handler;
+        Assert.True(e.HasHandler);
+        e.MyEvent -= Handler2;
+        Assert.False(e.HasHandler);
+    }
+
+    [Fact]
+    public void RemoveEventHandlers_CanRemoveInstanceEventHandler() {
+        var e = new EventClass();
+        e.MyEvent += Handler;
+        e.RemoveEventHandlers(nameof(e.MyEvent));
+        Assert.False(e.HasHandler);
+    }
+
+    [Fact]
+    public void RemoveEventHandlers_RemovesAllInstanceEventHandlers() {
+        var e = new EventClass();
+        e.MyEvent += Handler;
+        e.MyEvent += Handler2;
+        e.RemoveEventHandlers(nameof(e.MyEvent));
+        Assert.False(e.HasHandler);
+    }
+
+    [Fact]
+    public void RemoveEventHandlers_CanRemoveStaticEventHandlers() {
+        var e = new EventClass();
+        EventClass.MyStaticEvent += Handler;
+        Assert.True(EventClass.HasStaticHandler);
+        e.RemoveEventHandlers(nameof(EventClass.MyStaticEvent));
+        Assert.False(EventClass.HasStaticHandler);
+    }
+
+    [Fact]
+    public void RemoveEventHandlers_CanRemoveInheritedInstanceEventHandler() {
+        var e = new EventClassChild();
+        e.MyEvent += Handler;
+        Assert.True(e.HasHandler);
+        e.RemoveEventHandlers(nameof(e.MyEvent));
+        Assert.False(e.HasHandler);
+    }
+    [Fact]
+    public void RemoveEventHandlers_CanRemoveInstanceEventHandlerInInheritedClass() {
+        var e = new EventClassChild();
+        e.InheritedEvent += Handler;
+        Assert.True(e.HasInheritedHandler);
+        e.RemoveEventHandlers(nameof(e.InheritedEvent));
+        Assert.False(e.HasInheritedHandler);
+    }
+
+
+    void Handler(object? sender, EventArgs e) { }
+    void Handler2(object? sender, EventArgs e) { }  
+
+    public class EventClass {
+
+        public event EventHandler<EventArgs>? MyEvent;
+        public bool HasHandler => MyEvent?.GetInvocationList().Any() == true;
+
+        public static event EventHandler<EventArgs>? MyStaticEvent;
+        public static bool HasStaticHandler => MyStaticEvent?.GetInvocationList().Any() == true;
+    }
+
+    public class EventClassChild : EventClass {
+        public event EventHandler<EventArgs>? InheritedEvent;
+        public bool HasInheritedHandler => InheritedEvent?.GetInvocationList().Any() == true;
+    }
+    
+}
---
+++ Loudenvier.Utils/Loudenvier.Utils/Events/EventsExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Loudenvier.Utils.Events;
+
+/// <summary>
+/// A few utility methods/extensions that deal with .NET event handling and dispatching mechanisms.
+/// </summary>
+public static class EventsExtensions
+{
+
+    /// <summary>
+    /// Removes all event handlers from the <see cref="event"/> named <paramref name="EventName"/>.
+    /// </summary>
+    /// <param name="obj">The object from which the event handlers will be removed.</param>
+    /// <param name="EventName">The name of the event from which to remove all event handlers.</param>
+    public static void RemoveEventHandlers(this object obj, string EventName) {
+        const BindingFlags bindings = BindingFlags.IgnoreCase | BindingFlags.Public |
+            BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+        if (obj is null)
+            return;
+
+        EventInfo? ei = obj.GetType().GetEvent(EventName, bindings);
+
+        if (ei?.DeclaringType is not null) { 
+            FieldInfo? fi = ei.DeclaringType.GetField(ei.Name, bindings);
+            if (fi?.GetValue(obj) is Delegate mdel) {
+                foreach (Delegate del in mdel.GetInvocationList())
+                    ei.RemoveEventHandler(obj, del);
+            }
+        }
+    }
+
+}
