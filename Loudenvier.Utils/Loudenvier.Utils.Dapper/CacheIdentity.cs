02f939f181002a8dd4d383cb4c74d666cda63824
Felipe Machado <loudenvier@gmail.com>
2025-09-17 16:54:22 -0300

    Added DapperCached utility

---
+++ Loudenvier.Utils/Loudenvier.Utils.Dapper/CacheIdentity.cs
@@ -0,0 +1,41 @@
+﻿using SpookilySharp;
+using System.Text.Json;
+
+namespace Loudenvier.Utils.Dapper;  
+
+public class CacheIdentity: IEquatable<CacheIdentity>
+{
+    public CacheIdentity(string sql, object? param = null) {
+        if (string.IsNullOrWhiteSpace(sql))
+            throw new ArgumentNullException(nameof(sql));
+        Sql = sql;
+        this.Param = param;
+        CalculateHash();
+    }
+    public object? Param { get; }
+    public string Sql { get; }
+
+    private void CalculateHash() {
+        // TODO: Make Hashcode independent from Param field order!
+        var s = string.Concat(Sql, Serialize(Param));
+        Key = s.SpookyHash128();
+    }
+    public HashCode128 Key { get; private set; }
+    public override string ToString() => Key.ToString();
+    public static implicit operator string(CacheIdentity id) => id.ToString();
+
+    #region (In)Equality
+
+    public static bool operator ==(CacheIdentity left, CacheIdentity right) => left.Equals(right);
+    public static bool operator !=(CacheIdentity left, CacheIdentity right) => !(left == right);
+    public bool Equals(CacheIdentity? o) {
+        if (o is null) return false;
+        return ReferenceEquals(this, o) || Key.Equals(o.Key) && Sql.Equals(o.Sql) && (Param?.Equals(Param) ?? true);
+    }
+    public override bool Equals(object? o) => Equals(o as CacheIdentity);
+    public override int GetHashCode() => unchecked((int)Key.Hash1);
+
+    #endregion
+
+    static string Serialize(object? o) => JsonSerializer.Serialize(o);
+}
---
+++ Loudenvier.Utils/Loudenvier.Utils.Dapper/DapperCached.cs
@@ -0,0 +1,65 @@
+﻿using System.Data;
+using Loudenvier.Utils.Dapper;
+using Microsoft.Extensions.Caching.Memory;
+
+#pragma warning disable IDE0130 // Namespace does not match folder structure
+namespace Dapper;
+#pragma warning restore IDE0130 // Namespace does not match folder structure
+
+/*public static class DapperCached
+{
+    public static TimeSpan DefaultSlidingExpiration = TimeSpan.FromMinutes(5);
+    public static IEnumerable<T> GetFromCache<T>(CacheIdentity identity) 
+        => MemoryCache.Default.Get(identity) as IEnumerable<T>;
+
+    public static void AddToCache(CacheIdentity identity, object data, TimeSpan slidingExpiration) 
+        => MemoryCache.Default.Set(identity, data, new CacheItemPolicy { SlidingExpiration = slidingExpiration });
+
+    public static IEnumerable<T> QueryCached<T>(this IDbConnection conn, string sql, object param = null, bool skipCache = false, TimeSpan? slidingCacheExpiration = null) {
+        CacheIdentity identity = null;
+        if (!skipCache) {
+            identity = new CacheIdentity(sql, param);
+            var data = GetFromCache<T>(identity);
+            if (data != null)
+                return data;
+        }
+        var results = conn.Query<T>(sql, param);
+        AddToCache(identity ?? new CacheIdentity(sql, param), results, slidingCacheExpiration ?? DefaultSlidingExpiration);
+        return results;
+    }
+}*/
+
+public static class DapperCached
+{
+    public static TimeSpan DefaultSlidingExpiration = TimeSpan.FromMinutes(5);
+
+    static readonly Lazy<MemoryCache> lazyCache = new Lazy<MemoryCache>(() => new MemoryCache(new MemoryCacheOptions()));
+
+    // Create a singleton MemoryCache instance
+    private static MemoryCache Cache => lazyCache.Value;
+
+    public static IEnumerable<T>? GetFromCache<T>(CacheIdentity identity) 
+        => Cache.TryGetValue(identity, out IEnumerable<T>? cachedData) ? cachedData : null;
+
+    public static void AddToCache(CacheIdentity identity, object data, TimeSpan slidingExpiration) {
+        var options = new MemoryCacheEntryOptions {
+            SlidingExpiration = slidingExpiration
+        };
+        Cache.Set(identity, data, options);
+    }
+
+    public static IEnumerable<T> QueryCached<T>(this IDbConnection conn, string sql, object? param = null, bool skipCache = false, TimeSpan? slidingCacheExpiration = null) {
+        CacheIdentity? identity = null;
+
+        if (!skipCache) {
+            identity = new CacheIdentity(sql, param);
+            var data = GetFromCache<T>(identity);
+            if (data != null)
+                return data;
+        }
+
+        var results = conn.Query<T>(sql, param);
+        AddToCache(identity ?? new CacheIdentity(sql, param), results, slidingCacheExpiration ?? DefaultSlidingExpiration);
+        return results;
+    }
+}
