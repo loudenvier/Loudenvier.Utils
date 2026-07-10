3483d54b24f793cf343e562286d38778e6dd5a62
Felipe Machado <loudenvier@gmail.com>
2026-07-10 15:54:42 -0300

    Fix bit rotations and remove unused using
    Remove an unused System.Drawing using in IdGenerator.cs. Update BitOperations to mask shift amounts ((32 - offset) & 31 and (64 - offset) & 63) to avoid undefined shifts for out-of-range offsets, and replace TARGET_64BIT compile-time branches with runtime UIntPtr.Size since NET STANDARD 2 can't know in advance the target.

--- Loudenvier.Utils/Loudenvier.Utils/Numerics/BitOperations.cs
+++ Loudenvier.Utils/Loudenvier.Utils/Numerics/BitOperations.cs
@@ -2,9 +2,7 @@
 
 namespace System.Numerics;
 
-
 #if NETSTANDARD2_0
-
 /// <summary>
 /// Backport to NETSTANDARD 2.0 of some of the functionality in System.Numerics.BitOperations 
 /// found in NETSTANDARD 2.1 and above.
@@ -21,7 +19,7 @@ public static class BitOperations
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint RotateLeft(uint value, int offset)
-        => (value << offset) | (value >> (32 - offset));
+        => (value << offset) | (value >> ((32 - offset) & 31));
 
     /// <summary>
     /// Rotates the specified value left by the specified number of bits.
@@ -33,7 +31,7 @@ public static class BitOperations
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong RotateLeft(ulong value, int offset)
-        => (value << offset) | (value >> (64 - offset));
+        => (value << offset) | (value >> ((64 - offset) & 63));
 
     /// <summary>
     /// Rotates the specified value left by the specified number of bits.
@@ -45,13 +43,10 @@ public static class BitOperations
     /// and any value outside the range [0..63] is treated as congruent mod 64 on a 64-bit process.</param>
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static nuint RotateLeft(nuint value, int offset) {
-#if TARGET_64BIT
-        return (nuint)RotateLeft((ulong)value, offset);
-#else
-        return (nuint)RotateLeft((uint)value, offset);
-#endif
-    }
+    public static nuint RotateLeft(nuint value, int offset) 
+        => UIntPtr.Size == 8 
+            ? (nuint)RotateLeft((ulong)value, offset)
+            : RotateLeft((uint)value, offset);
 
     /// <summary>
     /// Rotates the specified value right by the specified number of bits.
@@ -63,7 +58,7 @@ public static class BitOperations
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static uint RotateRight(uint value, int offset)
-        => (value >> offset) | (value << (32 - offset));
+        => (value >> offset) | (value << ((32 - offset) & 31));
 
     /// <summary>
     /// Rotates the specified value right by the specified number of bits.
@@ -75,7 +70,7 @@ public static class BitOperations
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static ulong RotateRight(ulong value, int offset)
-        => (value >> offset) | (value << (64 - offset));
+        => (value >> offset) | (value << ((64 - offset) & 63));
 
     /// <summary>
     /// Rotates the specified value right by the specified number of bits.
@@ -87,13 +82,9 @@ public static class BitOperations
     /// and any value outside the range [0..63] is treated as congruent mod 64 on a 64-bit process.</param>
     /// <returns>The rotated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    public static nuint RotateRight(nuint value, int offset) {
-#if TARGET_64BIT
-        return (nuint)RotateRight((ulong)value, offset);
-#else
-        return (nuint)RotateRight((uint)value, offset);
-#endif
-    }
+    public static nuint RotateRight(nuint value, int offset) 
+        => UIntPtr.Size == 8
+            ? (nuint)RotateRight((ulong)value, offset)
+            : RotateRight((uint)value, offset);
 }
-
 #endif
